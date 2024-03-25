49f0a30f713b018c6a6350f3c93e4a266ef844c9
Mark Gamez <markagamez@gmail.com>
2024-03-24 19:25:20 -0700

    setup basic authentication

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|string|min:6',
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        $token = $user->createToken('Lomodoro')->plainTextToken;
+
+        return response()->json(['message' => 'User created successfully', 'user' => $user, 'token' => $token], 201);
+    }
+}
